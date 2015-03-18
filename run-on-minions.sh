8d0c2d0d04e0c605c80803cdafbb3bbf93236079
Roy van de Water <dev+roy@integrumtech.com>
2015-03-18 13:33:42 -0700

    Setup more complex file system

--- run-on-minions.sh
+++ run-on-minions.sh
@@ -1,6 +1,7 @@
 sudo apt-get install -y nfs-common
-sudo mount -t nfs -o proto=tcp,port=2049 172.20.0.9:/export/data /data
-
+sudo mkdir -p /data/read-only /data/read-write
+sudo mount -t nfs -o proto=tcp,port=2049 172.20.0.9:/export/read-only /data/read-only
+sudo mount -t nfs -o proto=tcp,port=2049 172.20.0.9:/export/read-write /data/read-write
 
 # To unmount
 sudo umount /data
